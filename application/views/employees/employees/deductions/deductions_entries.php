2b5b9b62ea024f6424201c958c6da48dd3b18f49
Chester Alan <chesteralantagudin@gmail.com>
2017-07-04 17:28:30 +0800

    Updates

--- application/views/employees/employees/deductions/deductions_entries.php
+++ application/views/employees/employees/deductions/deductions_entries.php
@@ -50,7 +50,11 @@ $total = 0;
             foreach($deductions as $deduction) { ?>
               <tr id="deduction-<?php echo $deduction->id; ?>">
                 <td><?php echo $deduction->payroll_name; ?></td>
-                <td class="text-right"><?php echo number_format($deduction->ped_amount,2); $total += $deduction->ped_amount; ?></td>
+                <td class="text-right">
+                <a class="ajax-modal-inner" href="<?php echo site_url("payroll_deductions/edit/{$deduction->ped_id}/{$output}") . '?next=' . (($this->input->get('next'))?$this->input->get('next'):"employees_deductions/view/{$employee->name_id}"); ?>">
+                <?php echo number_format($deduction->ped_amount,2); $total += $deduction->ped_amount; ?>
+                </a>
+                </td>
               <?php if( hasAccess('employees', 'employees', 'edit') ) { ?>
 <?php if( !$this->input->get('no_action') ) { ?>
                 <td>
